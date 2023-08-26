e86308d069dde6d112af39835cd729311232303c
AKuchko <vasco_da_gama1469@mail.ru>
2023-08-26 15:50:48 +0700

    setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL,
+} from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +16,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/global.scss";`
+      }
+    }
+  },
 })
